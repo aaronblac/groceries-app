f40d8ecddbbdb0bea3834d8651bbb5aef3d5e95b
aaron <aaronblac@gmail.com>
2024-04-05 23:18:37 -0700

    update with CRUD operations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { GroceriesServiceProvider } from '../providers/groceries-service/groceries-service';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    GroceriesServiceProvider,
+    GroceriesServiceProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, ToastController, AlertController } from 'ionic-angular';
+import { GroceriesServiceProvider } from '../../providers/groceries-service/groceries-service';
 
 @Component({
   selector: 'page-home',
@@ -9,42 +10,36 @@ export class HomePage {
 
   title = "Grocery List"
 
-  items = [
-    {
-      name:"Milk",
-      quantity: 2
-    },
-    {
-      name:"Eggs",
-      quantity: 12
-    },
-    {
-      name:"Bread",
-      quantity: 1
-    },
-    {
-      name:"Coffee",
-      quantity: 2
-    },
-    {
-      name:"Soda",
-      quantity: 2
-    }
-  ]
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController, public dataService: GroceriesServiceProvider) {
+  }
 
-
-  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController) {
+  loadItems(){
+    return this.dataService.getItems();
   }
 
   handleRemoveItem(item, index){
     if (item){
-      this.items.splice(index, 1)
 
+      this.dataService.removeItem(index);
       const toast = this.toastCtrl.create({
         message: `${item.name} was removed from the list`,
         duration: 3000
       });
       toast.present()
+    }
+
+  }
+
+  handleEditItem(item, index){
+    if (item){
+      console.log("Edit item", item, index);
+
+      const toast = this.toastCtrl.create({
+        message: `Editing item - ${item.name}`,
+        duration: 3000
+      });
+      toast.present()
+      this.showEditItemPrompt(item, index)
     }
 
   }
@@ -79,9 +74,49 @@ export class HomePage {
           text: 'Save',
           handler: item => {
             console.log('Saved clicked');
-            this.items.push(item);
+            this.dataService.addItem(item);
             const toast = this.toastCtrl.create({
               message: `${item.name} was added to the list`,
+              duration: 3000
+            });
+            toast.present()
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  showEditItemPrompt(item, index) {
+    const prompt = this.alertCtrl.create({
+      title: 'Grocery List',
+      message: "Edit your item",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name',
+          value: item.name
+        },
+        {
+          name: 'quantity',
+          placeholder: 'Quantity',
+          value: item.quantity
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: item => {
+            console.log('Saved clicked');
+            this.dataService.editItem(item,index);
+            const toast = this.toastCtrl.create({
+              message: `Your item has been edited`,
               duration: 3000
             });
             toast.present()
